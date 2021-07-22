dd61c4e298f43fa11eb8bbf73c1f8b6026b10edc
fang <649392395@qq.com>
2021-07-22 18:12:50 +0800

    muc

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,62 @@
+/* eslint-disable vue/require-v-for-key */
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{name}}</h1>
+    <div class="">
+ <audio autoplay controls :src="this.songdata.url"></audio>
+    </div>
+    
+    <input type="text" v-model="keywords"   @keyup.enter="fun" />
+    <button @click="fun">搜索</button>
+  <ul>
+    <li v-for="site in songs" :key="site.id">
+      <span>{{ site.name }}</span>
+      <span @click="song(site.id)">{{site.id }}</span>
+    </li>
+  </ul>
+  
+
+
+  
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      name:'',
+      keywords:'',
+      songs:'',
+      id:'',
+      songdata:'',
+      url:''
+    }
+  },
+  methods:{
+    fun(){
+      this.axios({
+        methods:'get',
+        url:'http://150.158.4.157:3002/search',
+          params:{keywords:this.keywords}
+      }).then(res=>{
+        this.songs=res.data.result.songs
+        console.log(res.data.result.songs);
+      })
+    },
+    song(id){
+       this.axios({
+        methods:'get',
+        url:'http://150.158.4.157:3002/song/url',
+          params:{id:id}
+      }).then(res=>{
+        this.songdata=res.data.data[0]
+        console.log( this.songdata);
+      })
+
+    },
+    gechi(){
+
+    }
+  }
+}
+</script>
